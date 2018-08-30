1699b2894cc2f0308b599d889941a2546020ed52
Dmitry Domanski <dmitry.domanski@gmail.com>
2018-08-30 17:30:56 +0300

    person's card styling

--- src/components/MemberCard/Portrait/Portrait.js
+++ src/components/MemberCard/Portrait/Portrait.js
@@ -3,8 +3,15 @@ import React from 'react';
 import classes from './Portrait.css';
 
 const portrait = (props) => {
+    // let imageUrl = 'url(../../../assets/' + props.url + '.jpg)'
+    const imageUrl = require(`../../../assets/${props.url}.jpg`);
+    const image = (<img src={imageUrl}  alt='a person pic'/>)
+    console.log(image);
+    
     return (
-        <div className={classes.Portrait}>{props.children}</div>
+        <div className={classes.Portrait}
+            style={{ backgroundImage: `url(${imageUrl})` }}>{props.children}</div>
+            // >{props.children}</div>
     )
 }
 
--- src/containers/FamilyTree/FamilyTree.js
+++ src/containers/FamilyTree/FamilyTree.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
 
-import MemberCard from '../../components/MemberCard/MemberCard';
+// import MemberCard from '../../components/MemberCard/MemberCard';
+import NewMemberCard from '../../components/NewMemberCard/NewMemberCard';
 import Aux from '../../hoc/Aux/Aux';
 
 class FamilyTree extends Component {
     state = {
         members: [
             { id: 1, firstName: 'Dmitry', lastName: 'Domanski', gender: 'male', maidenName: '', matrimonialStatus: 'm', birthDate: '06/10/1982', deathDate: null },
-            { id: 2, firstName: 'Anastasia', lastName: 'Domanski', gender: 'male', maidenName: 'Shchurok', matrimonialStatus: 'm', birthDate: '17/01/1982', deathDate: null },
-            { id: 3, firstName: 'Vera', lastName: 'Domanski', gender: 'female', maidenName: null , matrimonialStatus: 'k', birthDate: '23/10/2006', deathDate: null },
+            // { id: 2, firstName: 'Anastasia', lastName: 'Domanski', gender: 'male', maidenName: 'Shchurok', matrimonialStatus: 'm', birthDate: '17/01/1982', deathDate: null },
+            // { id: 3, firstName: 'Vera', lastName: 'Domanski', gender: 'female', maidenName: null , matrimonialStatus: 'k', birthDate: '23/10/2006', deathDate: null },
             { id: 4, firstName: 'Adam', lastName: 'Domanski', gender: 'male', maidenName: null, matrimonialStatus: 'k', birthDate: '06/10/2008', deathDate: null },
-            { id: 5, firstName: 'Daniel', lastName: 'Domanski', gender: 'male', maidenName: null, matrimonialStatus: 'k', birthDate: '14/12/2009', deathDate: null },
-            { id: 6, firstName: 'Roza', lastName: 'Domanski', gender: 'female', maidenName: null, matrimonialStatus: 'k', birthDate: '17/10/2017', deathDate: null },
-            { id: 7, firstName: 'Bella', lastName: 'Domanski', gender: 'female', maidenName: 'Talalay', matrimonialStatus: 'd', birthDate: '17/01/1934', deathDate: '07/09/2001' }
+            // { id: 5, firstName: 'Daniel', lastName: 'Domanski', gender: 'male', maidenName: null, matrimonialStatus: 'k', birthDate: '14/12/2009', deathDate: null },
+            // { id: 6, firstName: 'Roza', lastName: 'Domanski', gender: 'female', maidenName: null, matrimonialStatus: 'k', birthDate: '17/10/2017', deathDate: null },
+            // { id: 7, firstName: 'Bella', lastName: 'Domanski', gender: 'female', maidenName: 'Talalay', matrimonialStatus: 'd', birthDate: '17/01/1934', deathDate: '07/09/2001' }
+            { id: 8, firstName: 'Andrey', lastName: 'Domanski', gender: 'male', maidenName: null, matrimonialStatus: 'k', birthDate: '15/07/1960', deathDate: null },
+            { id: 9, firstName: 'Victor', lastName: 'Domanski', gender: 'male', maidenName: null, matrimonialStatus: 'k', birthDate: '11/04/1932', deathDate: '2010' }
 
         ]
     }
 
     render() {
-        let members = this.state.members.map(member => {
-            return <MemberCard
+
+        const sortedMembers = this.state.members.map(member => {
+            return { ...member, birthYear: +member.birthDate.split('').slice(-4).join('') }
+        }).sort((a, b) => a.birthYear - b.birthYear);
+
+        // console.log(sortedMembers);
+
+        let members = sortedMembers.map(member => {
+            return <NewMemberCard
+                url={member.id}
                 firstName={member.firstName}
                 lastName={member.lastName}
                 birthDate={member.birthDate}
